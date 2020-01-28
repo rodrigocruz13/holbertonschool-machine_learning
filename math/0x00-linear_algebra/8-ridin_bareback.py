80b2c4df84898065d34465e3a37ec28a28c8bbf2
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-01-28 15:10:05 -0500

    Fixed output

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -17,11 +17,9 @@ def mat_mul(mat1, mat2):
         for j in range(len(mat2[0])):
             row.append(0)
         new.append(row)
-    print (new)
 
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
                 new[i][j] += mat1[i][k] * mat2[k][j]
-            print(new[i][j])
     return new
