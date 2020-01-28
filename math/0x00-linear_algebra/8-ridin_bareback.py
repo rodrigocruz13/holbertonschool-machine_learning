c8f037c5be7ed8745a90915c7b417a21abcdf1da
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-01-28 15:07:10 -0500

    Fixed size of new matrix

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -12,16 +12,16 @@ def mat_mul(mat1, mat2):
         return None
 
     new = []
-    for i in range(len(mat1[0])):
+    for i in range(len(mat1)):
         row = []
-        for j in range(len(mat1[0])):
+        for j in range(len(mat2[0])):
             row.append(0)
         new.append(row)
+    print (new)
 
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
-                print(mat1[i][k], " x ", mat2[k][j])
                 new[i][j] += mat1[i][k] * mat2[k][j]
             print(new[i][j])
     return new
