d6226de29267c75d6bb5e1d683cae5e6d0c15a0d
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-01-28 14:07:15 -0500

    Task 0 - 7

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+def add_matrices2D(mat1, mat2):
+    """Matrix add function
+    """
+
+    if (type(mat1) is not list) or (type(mat2) is not list):
+        return None
+
+    row1 = len(mat1)
+    col1 = len(mat1[0])
+    row2 = len(mat2)
+    col2 = len(mat2[0])
+
+    if (row1 != row2) or (col1 != col2):
+        return None
+
+    new_matrix = []
+    for r_i in range(row1):
+        row_i = []
+        for c_i in range(col1):
+            row_i.append(mat1[r_i][c_i] + mat2[r_i][c_i])
+        new_matrix .append(row_i)
+
+    return new_matrix
