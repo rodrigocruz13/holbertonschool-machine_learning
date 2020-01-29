def09d339fd32df5744a0a24d9f0d50aa69132a3
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-01-28 19:44:14 -0500

    Task 13. import module location

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -2,12 +2,10 @@
 """
 Module used to use concatenate matrices in an axis
 """
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """Matrix concatenate operations
     """
-
-    import numpy as np
-
     return (np.concatenate((mat1, mat2), axis=axis))
