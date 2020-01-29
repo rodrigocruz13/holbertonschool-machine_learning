57dd89f4932f46bacaa3c70390f8ba0f7a6636d4
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-01-28 19:55:30 -0500

    Matmul

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module used to use matrix mult
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Matrix mat mul
+    """
+    return (np.matmul(mat1, mat2))
