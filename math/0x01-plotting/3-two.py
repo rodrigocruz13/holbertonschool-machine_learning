6c0f9ec54d6667831d7f4fd131665dcf4bdc1d90
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-01-30 13:39:24 -0500

    Task 3

---
+++ math/0x01-plotting/3-two.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 21000, 1000)
+r = np.log(0.5)
+t1 = 5730
+t2 = 1600
+y1 = np.exp((r / t1) * x)
+y2 = np.exp((r / t2) * x)
+
+color1 = "r--"  # red dash
+color2 = "g"  # green
+
+graph1, graph2 = plt.plot(x, y1, color1, x, y2, color2)
+
+plt.xlabel("Time (years)")
+plt.ylabel("Fraction Remaining")
+plt.title("Exponential Decay of C-14")
+plt.legend([graph1, graph2], ["C-14", "Ra-226"])
+
+plt.xlim(0, 20000)
+plt.ylim(0, 1)
+plt.show()
