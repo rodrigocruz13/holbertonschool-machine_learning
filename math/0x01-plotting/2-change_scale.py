0c3d9485a0d2df58db15ba4397f121f978df4827
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-01-30 13:03:05 -0500

    Task 2

---
+++ math/0x01-plotting/2-change_scale.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 28651, 5730)
+r = np.log(0.5)
+t = 5730
+y = np.exp((r / t) * x)
+
+plt.xlabel("Time (years)")
+plt.ylabel("Fraction Remaining")
+plt.title("Exponential Decay of C-14")
+
+plt.plot(x, y)
+plt.xlim(0, 28650)
+plt.yscale('log')
+plt.show()
