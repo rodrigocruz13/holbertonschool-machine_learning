a43b2b577cf28b9185ca86c2e73c1d2b264f45be
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-03 17:58:46 -0500

    all

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
 
-mean = [69, 0]
-cov = [[15, 8], [8, 15]]
-np.random.seed(5)
-x, y = np.random.multivariate_normal(mean, cov, 2000).T
-y += 180
 
-s = 5  # point size
-c = "m"  # mangenta color
-marker = 'o'  # marker circle
+def poly_integral(poly, C=0):
+    """
+    function that calculates the integral of a poly
+    Args:
+        poly (lst): poly is a list of coefficients representing a polynomial
+            The index is the power of x that the coefficient belongs to
+            Example: if f(x) = x^3 + 3x +5, poly is equal to [5, 3, 0, 1]
+        C (int): General constant 
+    Returns:
+        inte (lst): list with the coefficient of the integral
+    """
 
-plt.xlabel("Height (in)")
-plt.ylabel("Weight (lbs)")
-plt.title("Men's Height vs Weight")
-
-plt.scatter(x, y, s, c, marker)
-plt.show()
+    print (type(poly))
+    if type(poly) is not list:
+        return None
+    if type(poly) is None:
+        return None
+    if type(C) is None or type(C) is int or type(C) is float :
+        if len(poly) == 0:
+            return [C]
+        else:
+            inte = [0] * (len(poly) + 1)
+            for i in range(len(poly)):
+                temp = poly[i] / (i + 1)
+                if temp % 1 != 0:
+                    inte[i + 1] = float(poly[i])/(i+1)
+                else:
+                    inte[i +1] = int((poly[i])/(i+1))
+            inte[0] = C
+        return inte
+    return None
--- math/0x02-calculus/17-main.py
+++ math/0x02-calculus/17-main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
 poly_integral = __import__('17-integrate').poly_integral
-
 poly = [5, 3, 0, 1]
 print(poly_integral(poly))
-alexa@ubuntu:0x02-calculus$ ./17-main.py 
-[0, 5, 1.5, 0, 0.25]
