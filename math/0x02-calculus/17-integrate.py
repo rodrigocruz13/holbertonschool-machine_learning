63d325785fdec419cb0a8a0d88e4ec906beb8dc1
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-04 10:31:28 -0500

    pycodestyle

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -8,17 +8,16 @@ def poly_integral(poly, C=0):
         poly (lst): poly is a list of coefficients representing a polynomial
             The index is the power of x that the coefficient belongs to
             Example: if f(x) = x^3 + 3x +5, poly is equal to [5, 3, 0, 1]
-        C (int): General constant 
+        C (int): General constant
     Returns:
         inte (lst): list with the coefficient of the integral
     """
 
-    print (type(poly))
     if type(poly) is not list:
         return None
     if type(poly) is None:
         return None
-    if type(C) is None or type(C) is int or type(C) is float :
+    if type(C) is None or type(C) is int or type(C) is float:
         if len(poly) == 0:
             return [C]
         else:
@@ -26,9 +25,9 @@ def poly_integral(poly, C=0):
             for i in range(len(poly)):
                 temp = poly[i] / (i + 1)
                 if temp % 1 != 0:
-                    inte[i + 1] = float(poly[i])/(i+1)
+                    inte[i + 1] = float(poly[i])/(i + 1)
                 else:
-                    inte[i +1] = int((poly[i])/(i+1))
+                    inte[i + 1] = int((poly[i])/(i + 1))
             inte[0] = C
         return inte
     return None
