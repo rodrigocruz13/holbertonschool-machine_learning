e030cdcf0dd14931853fcdd4c12461ff8fa6c5bb
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-04 12:55:56 -0500

    Fixed function

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -20,20 +20,5 @@ def summation_i_squared(n):
     if n is None or type(n) is not int or n < 1:
         return None
     else:
-        return int(sum_recursive(n))
-
-
-def sum_recursive(n):
-    """
-    function that calculates sum recursevily
-    Args:
-        n (int): is the stopping condition
-
-    Returns:
-        sum (int): sum
-    """
-
-    if n < 1:
-        return 0
-    else:
-        return (n * n) + sum_recursive(n - 1)
+        x = n * (n + 1) * (2 * n + 1) / 6
+        return int(x)
