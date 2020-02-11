dfdc9ac98ad013da2d7e69afdd0ed7b385fa23cd
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-11 02:56:31 -0500

    task 1

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -17,8 +17,8 @@ class Poisson:
             λ
         """
         self.lambtha = lambtha
-        π = 3.1415926536
-        e = 2.7182818285
+        self.π = 3.1415926536
+        self.e = 2.7182818285
 
         λ = float(lambtha)
 
@@ -34,7 +34,7 @@ class Poisson:
             λ = float(sum(data) / len(data))
             self.lambtha = λ
 
-    def erf(x):
+    def erf(self, x):
         """
         is the "error function" encountered in integrating the normal distr
         (which is a normalized form of the Gaussian function). Erf can also
@@ -45,7 +45,7 @@ class Poisson:
                (float): An error aproximation using the Maclaurin series.
         """
 
-        π = 3.1415926536
+        π = self.π
         root_π = π**(1/2)
         x3 = x**3
         x5 = x**5
@@ -53,3 +53,38 @@ class Poisson:
         x9 = x**9
 
         return (2/root_π) * x - x3/3 + x5/10 - x7/42 + x9/216
+
+    def factorial(self, k):
+        """
+        Returns the factorial of a number.
+        Args:
+            k (int): Value
+        Returns:
+            num! (int): An error aproximation using the Maclaurin series.
+        """
+        n = int(k)
+        fact = 1
+
+        for n in range(1, n + 1):
+            fact = fact * n
+        return fact
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of “successes”
+        Args:
+            k (float): Number of “successes”
+
+        Returns:
+               PMF (float): The PMF value for k.
+        """
+
+        if k <= 0:
+            return 0
+
+        k = int(k)
+        λ = self.lambtha
+        k_f = self.factorial(k)
+        e = self.e
+
+        return (λ ** k) * (e ** -λ) / k_f
