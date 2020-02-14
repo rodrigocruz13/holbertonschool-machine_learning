3fc90d56a5fe06b221f8dfd95e9ad41771511a1e
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-14 17:26:41 -0500

    Task 1

---
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Class Neuron
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """ Class """
+
+    def __init__(self, nx):
+        """
+        Initialize Neuron
+        Args:
+            - nx: nx is the number of input features to the neuron
+        Public attributes:
+        - W: The weights vector for the neuron.
+              It is initialized with a random normal distribution.
+        - b: The bias for the neuron. Upon instantiation.
+             It is initialized to 0.
+        - A: The activated output of the neuron (prediction).
+            It is initialized to 0.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        #  Draw random samples from a normal dist.
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        getter method
+        Args:
+            - self
+        Return:
+        - __W: The value of the proate attribute __W.
+        """
+        return self.__W
+
+    @property
+    def A(self):
+        """
+        getter method
+        Args:
+            - self
+        Return:
+        - __A: The value of the proate attribute __A.
+        """
+        return self.__A
+
+    @property
+    def b(self):
+        """
+        getter method
+        Args:
+            - self
+        Return:
+        - __b: The value of the proate attribute __b.
+        """
+        return self.__b
