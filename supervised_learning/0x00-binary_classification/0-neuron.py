e7e5464bd2c4b72fbbe5842ceec3d09d18261139
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-14 16:14:22 -0500

    Task 0

--- supervised_learning/0x00-binary_classification/0-neuron.py
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -22,13 +22,13 @@ class Neuron:
         - A: The activated output of the neuron (prediction).
             It is initialized to 0.
         """
-        if not isinstance(nx, int):
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
 
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.nx = nx
         self.W = np.random.normal(size=nx)
+        #  Draw random samples from a normal dist.
         self.b = 0
         self.A = 0
