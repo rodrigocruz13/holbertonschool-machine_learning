a2fece7ebb6af277270efab5bf4b9a3189313017
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-13 23:05:51 -0500

    task 0

---
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Class Neuron
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """ Class """
+
+    def __init__(self, nx):
+        """
+        Initialize Neuron
+        Args:
+            - nx: nx is the number of input features to the neuron
+        Public attributes:
+        - W: The weights vector for the neuron.
+              It is initialized with a random normal distribution.
+        - b: The bias for the neuron. Upon instantiation.
+             It is initialized to 0.
+        - A: The activated output of the neuron (prediction).
+            It is initialized to 0.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.normal(size=nx)
+        self.b = 0
+        self.A = 0
