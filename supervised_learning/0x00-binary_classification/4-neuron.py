18175052f7a6f87fdd045549f392d98157821d0a
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-17 02:19:40 -0500

    Task 4

---
+++ supervised_learning/0x00-binary_classification/4-neuron.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+"""
+Class Neuron
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """ Class """
+
+    def __init__(self, nx):
+        """
+        Initialize Neuron
+        Args:
+            - nx: nx is the number of input features to the neuron
+        Public attributes:
+        - W: The weights vector for the neuron.
+              It is initialized with a random normal distribution.
+        - b: The bias for the neuron. Upon instantiation.
+             It is initialized to 0.
+        - A: The activated output of the neuron (prediction).
+            It is initialized to 0.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        #  Draw random samples from a normal dist.
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        getter method
+        Args:
+            - self
+        Return:
+        - __W: The value of the attribute __W.
+        """
+        return self.__W
+
+    @property
+    def A(self):
+        """
+        getter method
+        Args:
+            - self
+        Return:
+        - __A: The value of the attribute __A.
+        """
+        return self.__A
+
+    @property
+    def b(self):
+        """
+        getter method
+        Args:
+            - self
+        Return:
+        - __b: The value of the attribute __b.
+        """
+        return self.__b
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron
+        Updates the private attribute __A
+        Uses a sigmoid activation function
+        Arguments:
+        - X is a numpy.ndarray with shape (nx, m) that contains the input data
+          - nx (int) is the number of input features to the neuron
+          - m (int) is the number of examples
+        Return:
+        - __A: The value of the attribute __A.
+        """
+        # z = w.X + b
+        z = np.matmul(self.W, X) + self.b
+        forward_prop = 1 / (1 + np.exp(-1 * z))
+        self.__A = forward_prop
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        Arguments:
+        - Y is a numpy.ndarray with shape (1, m) that contains the correct
+            labels for the input data
+        - A is a numpy.ndarray with shape (1, m) containing the activated
+        output of the neuron for each example
+        Return:
+        - cost: the cost
+        Answer from: https://bit.ly/37x9YzM
+        """
+        m = Y.shape[1]
+        j = - (1 / m)
+
+        Ä = 1.0000001 - A
+        Ÿ = 1 - Y
+        cost = j * np.sum(np.multiply(Y, np.log(A)) +
+                          np.multiply(Ÿ, np.log(Ä)))
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        Calculates the cost of the model using logistic regression
+        Arguments:
+        - Y is a numpy.ndarray with shape (1, m) that contains the correct
+            labels for the input data
+        - A is a numpy.ndarray with shape (1, m) containing the activated
+        output of the neuron for each example
+        Return:
+        - Prediction: The prediction should be a numpy.ndarray with shape
+                      (1, m) containing the predicted labels for each
+                      example. The label values should be 1 if the output
+                      of the network is >= 0.5 and 0 otherwise
+        - cost: the cost
+        Answer from: https://bit.ly/37x9YzM
+        """
+
+        # Generate forward propagation.
+        # This creates the value of each activation
+        self.forward_prop(X)
+
+        # Calculate cost
+        cost = self.cost(Y, self.__A)
+
+        # Generate a matrix of size (1, m) and generate labels
+        # 1 if the output of the network is >= 0.5 and 0 otherwise
+
+        labels = self.__A.copy()
+
+        for i in range(len(self.__A)):
+            for j in range(len(self.__A[0])):
+                if (self.__A[i][j] >= 0.5):
+                    labels[i][j] = 1
+                else:
+                    labels[i][j] = 0
+        return (labels, cost)
