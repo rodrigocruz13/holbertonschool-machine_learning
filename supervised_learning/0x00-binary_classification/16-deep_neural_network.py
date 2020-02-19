9eb11e20ed1e7b611e59baab70f6bb1e1d7dd4e2
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-18 23:53:32 -0500

    Task 16

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -32,18 +32,21 @@ class DeepNeuralNetwork:
         """
 
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+            raise TypeError("nx must be a integer")
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) < 1:
+            raise ValueError("nx must be positive")
+        if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
-        for a_layer in layers:
-            if not isinstance(a_layer, int) or a_layer < 1:
-                raise TypeError("layers must be a list of positive integers")
+        if min(layers) < 1:
+            raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
-        self.weights = {"W1": np.random.randn(layers[0], nx), "b1": np.zeros((layers[0], 1))}
+        self.weights = {
+            "W1": np.random.randn(
+                layers[0], nx), "b1": np.zeros(
+                (layers[0], 1))}
         for i in range(1, self.L):
-            self.weights[("W{}".format(i+1))] = np.random.randn(layers[i], layers[i-1]) * np.sqrt(2/layers[i-1])
-            self.weights[("b{}".format(i+1))] = np.zeros((layers[i], 1))
+            self.weights[("W{}".format(i + 1))] = np.random.randn(layers[i],
+                                                                  layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+            self.weights[("b{}".format(i + 1))] = np.zeros((layers[i], 1))
