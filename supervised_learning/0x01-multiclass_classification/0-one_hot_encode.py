f6021d8cfd800473c22ac29ad088c34b7f6ee5ce
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-20 17:56:34 -0500

    Task 0

---
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module to work with milticlass classification
+"""
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+        Converts a numeric label vector into a one-hot matrix:
+        Args:
+            - Y is a numpy.ndarray with shape (m, )
+            containing numeric class labels
+            - m is the number of examples
+
+        Returns:
+            a one-hot encoding of Y with shape (classes, m),
+            None on failure
+    """
+
+    if (classes > 0 and Y.shape[0] > 0):
+        # create empty matrix
+        encoded_Y = np.zeros((classes, Y.shape[0]))
+
+        # Hot encoding
+        for i in range(Y.shape[0]):
+            encoded_Y[Y[i]][i] = 1
+        return encoded_Y
+
+    else:
+        return None
