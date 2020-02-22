6fd642da160c14b7eed1b142c959b491dc385094
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-22 15:10:19 -0500

    Added more cases

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -12,21 +12,21 @@ def one_hot_decode(one_hot):
         Args:
             - one_hot is a one-hot encoded numpy.ndarray with shape (classes, m)
             - m is the number of examples
-
         Returns:
             a one-hot encoding of Y with shape (classes, m),
             None on failure
     """
-    if type(one_hot) == np.ndarray:
-        if (one_hot.shape[0] > 0 and type(one_hot.shape[0]) == int):
-            lenght_ = one_hot.shape[0]
-            a_list = []
+    if isinstance(one_hot, np.ndarray):
+        if (one_hot.shape[0] > 0 and isinstance(one_hot.shape[0], int)):
+            if (np.max(one_hot) > 0):
+                lenght_ = one_hot.shape[0]
+                a_list = []
 
-            for i in range(lenght_):
-                column = one_hot[:, i]
-                key = np.unique(column, return_index=True, axis=None)
-                a_list.append(key[1][1])
+                for i in range(lenght_):
+                    column = one_hot[:, i]
+                    key = np.unique(column, return_index=True, axis=None)
+                    a_list.append(key[1][1])
 
-            one_hot_decode = np.asarray(a_list)
+                one_hot_decode = np.asarray(a_list)
             return (one_hot_decode)
     return None
