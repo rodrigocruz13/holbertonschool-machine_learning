e6da6deca94f3f2dce126b7f2a6b47a3774db9f7
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-23 10:58:55 -0500

    Task 16

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -28,41 +28,35 @@ class DeepNeuralNetwork:
             saved in the weights dictionary using the key b{l} where {l}
             is the hidden layer the bias belongs to
         """
+
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+            raise TypeError('nx must be an integer')
 
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
+            raise ValueError('nx must be a positive integer')
 
         if not isinstance(layers, list):
-            raise TypeError("layers must be a list of positive integers")
+            raise TypeError('layers must be a list of positive integers')
 
-        if layers[0] < 1:
-            raise ValueError("layers must be a list of positive integers")
+        if len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
 
-        if (np.where(layers[0] < 0, True, False)):
-            raise TypeError("layers must be a list of positive integers")
-
-        self.L = layers[0]
+        self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        "layer_size = ls"
-        "w=np.random.randn(ls[l],ls[l-1])*np.sqrt(2/ls[l-1])"
-
         for l in range(len(layers)):
-            if not isinstance(layers[l], int):
+            if not isinstance(layers[l], int) or layers[l] <= 0:
                 raise TypeError('layers must be a list of positive integers')
-
-            if layers[l] <= 0:
-                raise TypeError('layers must be a list of positive integers')
-
-            if l == 0:
-                he_et_tal = np.random.randn(layers[l], nx) * np.sqrt(2 / nx)
-
-            else:
-                he_et_tal = np.random.randn(layers[l], layers[l - 1])
-                he_et_tal = he_et_tal * np.sqrt(2 / layers[l - 1])
 
             self.weights["b" + str(l + 1)] = np.zeros((layers[l], 1))
-            self.weights["W" + str(l + 1)] = he_et_tal
+
+            if l == 0:
+                sq = np.sqrt(2 / nx)
+                heetal = np.random.randn(layers[l], nx) * sq
+                self.weights["W" + str(l + 1)] = heetal
+
+            else:
+                sq = np.sqrt(2 / layers[l - 1])
+                heetal = np.random.randn(layers[l], layers[l - 1]) * sq
+                self.weights["W" + str(l + 1)] = heetal
