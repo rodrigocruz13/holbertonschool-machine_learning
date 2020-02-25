be3c82d237e6afe2c9823dda7c23d2f887ce2b93
rodrigocruz13 <rodrigocruz13>
2020-02-25 18:25:46 -0500

    Task 3

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -18,8 +18,15 @@ def calculate_accuracy(y, y_pred):
         Returns: a tensor containing the decimal accuracy of the prediction
     """
 
+    # from Stackoverflow: https://bit.ly/390AzXw
+    # shorturl.at/mCF13
+
     ŷ = y_pred
-    equality = tf.equal(ŷ, y)
+
+    ŷ1 = tf.argmax(ŷ, 1)
+    y1 = tf.argmax(y, 1)
+
+    equality = tf.equal(ŷ1, y1)
     accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
 
     return accuracy
