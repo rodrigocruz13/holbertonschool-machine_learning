05f804ab8a8efd68e058c778b1141b4eab6d0e94
rodrigocruz13 <rodrigocruz13>
2020-03-05 13:56:40 -0500

    Task 1

---
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Module used to
+"""
+
+import numpy as np
+
+
+def sensitivity(confusion):
+    """
+    calculates the normalization (standardization) constants of a matrix:
+    Args:
+        - confusion: a confusion numpy.ndarray of shape (classes, classes)
+        where row indices represent the correct labels and column indices
+        represent the predicted labels
+            - classes is the number of classes
+    Returns:
+        numpy.ndarray of shape (classes,) with the sensitivity of each class
+    """
+
+    sensitivity = []
+    i = 0
+    for row in confusion:
+        positive = row[i]
+        false_positive = np.sum(row)
+        sensitivity.append(positive / false_positive)
+        i = i + 1
+
+    return (sensitivity)
