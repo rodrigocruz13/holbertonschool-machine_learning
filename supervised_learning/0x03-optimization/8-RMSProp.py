7d0eaf7b7c3f10ff9e2a1792c1c24445c16acee4
rodrigocruz13 <rodrigocruz13>
2020-03-05 09:58:41 -0500

    Task 8 with decay=B2

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -13,7 +13,7 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
     Args:
         loss     is the loss of the network
         alpha:   is the learning rate
-        beta2:   is the momentum weight
+        beta2:   is the RMSProp weight
         epsilon: is a small number to avoid division by zero
     Returns:
         the RMSProp optimization operation
@@ -26,7 +26,10 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
     # tf.train.RMSPropOptimizer
     # https://github.com/tensorflow/docs/blob/r1.12/site/en/api_docs/python/tf/train/RMSPropOptimizer.md
     # Optimizer that implements the RMSProp algorithm.
-    train = tf.train.RMSPropOptimizer(learning_rate=α, momentum=β2, epsilon=ε)
+
+    # train=tf.train.RMSPropOptimizer(learning_rate=α, momentum=β2, epsilon=ε)
+    # original and I dont undertand why decay = b2
+    train = tf.train.RMSPropOptimizer(learning_rate=α, decay=β2, epsilon=ε)
 
     # Args minimize(
     # - loss, A Tensor containing the value to minimize
