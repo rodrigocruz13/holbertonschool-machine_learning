a84596de9bff2c976d4ed34f0a2dfda3bdade95c
rodrigocruz13 <rodrigocruz13>
2020-03-05 13:11:47 -0500

    Task 0

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Module used to
+"""
+
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    creates a confusion matrix
+    Args:
+        - Labels is a one-hot numpy.ndarray of shape (m, classes)
+          containing the correct labels for each data point
+            - m is the number of data points
+            - classes is the number of classes
+        - logits is a one-hot numpy.ndarray of shape (m, classes)
+          containing the predicted labels
+    Returns:
+        Mean and standard deviation of each feature, respectively
+    """
+
+    # labels.shape = 50.000 x 10
+    # logits.shape = 50.0000 x 10
+    # Answer = 10 x 10  --->  labels.transpose
+
+    confusion = np.dot(labels.T, logits)
+
+    return (confusion)
