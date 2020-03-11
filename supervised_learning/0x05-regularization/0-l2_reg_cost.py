df2a82f8935fffb15f4c3581f517b5748e42c14f
rodrigocruz13 <rodrigocruz13>
2020-03-10 19:52:00 -0500

    Task 0

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -12,7 +12,8 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Args:
         - cost is the cost of the network without L2 regularization
         - lambtha is the regularization parameter
-        - weights is a dictionary of the weights and biases (numpy.ndarrays) of the neural network
+        - weights is a dictionary of the weights and biases (numpy.ndarrays)
+          of the neural network
         - L is the number of layers in the neural network
         - m is the number of data points used
     Returns:
