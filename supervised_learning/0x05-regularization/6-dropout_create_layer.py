6bd4f9ddb143cf0bd14a7c50aa90554ffa96a4dc
rodrigocruz13 <rodrigocruz13>
2020-03-11 20:07:02 -0500

    Task 6

---
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module used to
+"""
+
+import tensorflow as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """
+    creates a layer of a neural network using dropout
+    Args:
+        - prev is a tensor containing the output of the previous layer
+        - n is the number of nodes the new layer should contain
+        - activation is the activation funct that should be used on the layer
+        - keep_prob is the probability that a node will be kept
+    Returns:
+        the output of the new layer
+    """
+
+    # tf.contrib.layers.l2_regularizer
+    # Returns a funct that can be used to apply L2 regularization to weights.
+
+    raw_layer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+
+    output_tensor = tf.layers.Dense(units=n,
+                                    activation=activation,
+                                    kernel_initializer=raw_layer)
+
+    droput_layer = tf.layers.Dropout(keep_prob)
+    applied_layer = output_tensor(prev)
+    return(applied_layer)
