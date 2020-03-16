98ce8d5b097bf71aa040b1bdc6f027e166941994
rodrigocruz13 <rodrigocruz13>
2020-03-16 18:20:23 -0500

    Task 1

--- supervised_learning/0x06-keras/1-main.py
+++ supervised_learning/0x06-keras/1-main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
-build_model = __import__('0-sequential').build_model
-
+build_model = __import__('1-input').build_model
 
 if __name__ == '__main__':
-    network = build_model(784,  # features
-                          [256, 256, 10],  # layers
-                          ['tanh', 'tanh', 'softmax'],  # activations
-                          0.001,  # lambtha
-                          0.95)
+    network = build_model(
+        784, [
+            256, 256, 10], [
+            'tanh', 'tanh', 'softmax'], 0.001, 0.95)
     network.summary()
-    print(network.losses)  # fdflsddsd
+    print(network.losses)
