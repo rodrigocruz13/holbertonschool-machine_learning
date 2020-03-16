074becb1563f9b509968e342b02428035813373a
rodrigocruz13 <rodrigocruz13>
2020-03-16 17:38:38 -0500

    Task 0

---
+++ supervised_learning/0x06-keras/1-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+build_model = __import__('0-sequential').build_model
+
+
+if __name__ == '__main__':
+    network = build_model(784,  # features
+                          [256, 256, 10],  # layers
+                          ['tanh', 'tanh', 'softmax'],  # activations
+                          0.001,  # lambtha
+                          0.95)
+    network.summary()
+    print(network.losses)  # fdflsddsd
