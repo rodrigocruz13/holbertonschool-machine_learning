6ea814aa4a5f67ad7d88ee9ccb6e4efc7e618ae8
rodrigocruz13 <rodrigocruz13>
2020-03-18 23:55:03 -0500

    main

--- math/0x04-convolutions_and_pooling/4-main.py
+++ math/0x04-convolutions_and_pooling/4-main.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     dataset = np.load('../../supervised_learning/data/animals_1.npz')
     images = dataset['data']
     print(images.shape)
-    kernel = np.array([[[0, 0, 0], [-1, -1, -1], [0, 0, 0]], [[-1, -1, -1], [5, 5, 5], [-1, -1, -1]], [[0, 0, 0], [-1, -1, -1], [0, 0, 0]]])
+    kernel = np.array([[[0, 0, 0], [-1, -1, -1], [0, 0, 0]], [[-1, -1, -1],
+                                                              [5, 5, 5], [-1, -1, -1]], [[0, 0, 0], [-1, -1, -1], [0, 0, 0]]])
     images_conv = convolve_channels(images, kernel, padding='valid')
     print(images_conv.shape)
 
