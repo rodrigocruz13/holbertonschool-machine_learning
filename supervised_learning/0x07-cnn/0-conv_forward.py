d4a656ea4aa2c51ca37db94024ed3e848210cf58
rodrigocruz13 <rodrigocruz13>
2020-03-23 03:46:26 -0500

    Task 0

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -11,33 +11,33 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
     Function that performs forward propagation over a convolutional layer of
     a neural network:
     Args:
-        - A_prev is a numpy.ndarray with shape (m, h_prev, w_prev, c_prev)
-          containing the output of the previous layer
-            - m  is the number of examples
-            - h_prev is the height of the previous layer
-            - w_prev is the width of the previous layer
-            - c_prev is the number of channels in the previous layer
-        - W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the
-          kernels for the convolution
-            - kh is the filter height
-            - kw is the filter width
-            - c_prev is the number of channels in the previous layer
-            - c_new is the number of channels in the output
-        - b is a numpy.ndarray of shape (1, 1, 1, c_new) containing the biases
-          applied to the convolution
-        - activation is an activation function applied to the convolution
-        - padding is a string that is either same or valid, indicating the
-          type of padding used
-        - stride is a tuple of (sh, sw) containing the strides for the
-          convolution
-            - sh is the stride for the height
-            - sw is the stride for the width
+        - A_prev:   is a numpy.ndarray with shape (m, h_prev, w_prev, c_prev)
+                    containing the output of the previous layer
+                    - m  is the number of examples
+                    - h_prev is the height of the previous layer
+                    - w_prev is the width of the previous layer
+                    - c_prev is the number of channels in the previous layer
+        - W:        is a numpy.ndarray of shape (kh, kw, c_prev, c_new)
+                    containing the kernels for the convolution
+                    - kh is the filter height
+                    - kw is the filter width
+                    - c_prev is the number of channels in the previous layer
+                    - c_new is the number of channels in the output
+        - b:        is a numpy.ndarray of shape (1, 1, 1, c_new) containing
+                    the biases applied to the convolution
+        - activation: is an activation function applied to the convolution
+        - padding:  is a string that is either same or valid, indicating the
+                    type of padding used
+        - stride:   is a tuple of (sh, sw) containing the strides for the
+                    convolution
+                    - sh is the stride for the height
+                    - sw is the stride for the width
     Returns:
         the output of the convolutional layer
     """
 
     # num examples
-    n_examples = A_prev.shape[0]
+    m = A_prev.shape[0]
 
     # width and height of the previous layer
     h_prev = A_prev.shape[1]
@@ -56,17 +56,18 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
     s_w = stride[1]
 
     # output_height and output_width
-    o_h = h_prev - k_h + 1
-    o_w = w_prev - k_w + 1
-    if (padding == "same"):
-        o_h = h_prev / s_h
-        o_w = w_prev / s_h
+    o_h = ((h_prev - k_h + 1) // s_h)  # ceil function
+    o_w = ((w_prev - k_w + 1) // s_w)  # ceil function
 
-    # creating outputs of size: [n_examples,  o_h  ⊛  o_w  ⊛  c_output]
-    outputs = np.zeros((n_examples, o_h, o_w, c_output))
+    if (padding == "same"):
+        o_h = (h_prev // s_h) + 1  # ceil function
+        o_w = (w_prev // s_w) + 1  # ceil funtion
+
+    # creating outputs of size: [m,  o_h  ⊛  o_w  ⊛  c_output]
+    outputs = np.zeros((m, o_h, o_w, c_output))
 
     # vectorizing the n_images into an array (creating a new dimension)
-    imgs_arr = np.arange(0, n_examples)
+    imgs_arr = np.arange(0, m)
 
     # iterating over the output array and generating the convolution
     for x in range(o_h):
@@ -76,8 +77,12 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
                 y0 = y * s_w
                 x1 = x0 + k_h
                 y1 = y0 + k_w
-                outputs[imgs_arr, x, y, z] = np.sum(np.multiply(
-                    A_prev[imgs_arr, x0: x1, y0: y1], W[:, :, :, z]),
-                    axis=(1, 2, 3))
-    outputs += b
+
+                A_ = A_prev[imgs_arr, x0: x1, y0: y1]
+                W_ = W[:, :, :, z]
+                b_ = b[:, :, :, z]
+
+                outputs[imgs_arr, x, y, z] = activation(
+                    np.sum((A_ * W_), axis=(1, 2, 3)) + b_)
+
     return outputs
