076cba98c1ee36bb1377a25c3ec84f03a0ccb8c3
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-03-26 01:36:38 -0500

    main

--- supervised_learning/0x07-cnn/4-main.py
+++ supervised_learning/0x07-cnn/4-main.py
@@ -1,18 +1,47 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
+import matplotlib.pyplot as plt
 import numpy as np
-conv_backward = __import__('2-conv_backward').conv_backward
+import tensorflow as tf
+lenet5 = __import__('4-lenet5').lenet5
 
 if __name__ == "__main__":
     np.random.seed(0)
+    tf.set_random_seed(0)
     lib = np.load('../data/MNIST.npz')
     X_train = lib['X_train']
-    _, h, w = X_train.shape
-    X_train_c = X_train[:10].reshape((-1, h, w, 1))
-
-    W = np.random.randn(3, 3, 1, 2)
-    b = np.random.randn(1, 1, 1, 2)
-
-    dZ = np.random.randn(10, h - 2, w - 2, 2)
-    print(conv_backward(dZ, X_train_c, W, b, padding="valid"))
-
+    Y_train = lib['Y_train']
+    X_valid = lib['X_valid']
+    Y_valid = lib['Y_valid']
+    m, h, w = X_train.shape
+    X_train_c = X_train.reshape((-1, h, w, 1))
+    X_valid_c = X_valid.reshape((-1, h, w, 1))
+    x = tf.placeholder(tf.float32, (None, h, w, 1))
+    y = tf.placeholder(tf.int32, (None,))
+    y_oh = tf.one_hot(y, 10)
+    y_pred, train_op, loss, acc = lenet5(x, y_oh)
+    batch_size = 32
+    epochs = 10
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(epochs):
+            cost, accuracy = sess.run((loss, acc), feed_dict={x:X_train_c, y:Y_train})
+            cost_valid, accuracy_valid = sess.run((loss, acc), feed_dict={x:X_valid_c, y:Y_valid})
+            print("After {} epochs: {} cost, {} accuracy, {} validation cost, {} validation accuracy".format(epoch, cost, accuracy, cost_valid, accuracy_valid))
+            p = np.random.permutation(m)
+            X_shuffle = X_train_c[p]
+            Y_shuffle = Y_train[p]
+            for i in range(0, m, batch_size):
+                X_batch = X_shuffle[i:i+batch_size]
+                Y_batch = Y_shuffle[i:i+batch_size]
+                sess.run(train_op, feed_dict={x:X_batch, y:Y_batch})
+        cost, accuracy = sess.run((loss, acc), feed_dict={x:X_train_c, y:Y_train})
+        cost_valid, accuracy_valid = sess.run((loss, acc), feed_dict={x:X_valid_c, y:Y_valid})
+        print("After {} epochs: {} cost, {} accuracy, {} validation cost, {} validation accuracy".format(epochs, cost, accuracy, cost_valid, accuracy_valid))
+        Y_pred = sess.run(y_pred, feed_dict={x:X_valid_c, y:Y_valid})
+        print(Y_pred[0])
+        Y_pred = np.argmax(Y_pred, 1)
+        plt.imshow(X_valid[0])
+        plt.title(str(Y_valid[0]) + ' : ' + str(Y_pred[0]))
+        plt.show()
