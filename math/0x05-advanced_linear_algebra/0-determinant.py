4e5d7a88bd7e50707fc4c4aa6b3623e5639ceccb
rodrigocruz13 <rodrigocruz13>
2020-05-05 19:18:23 -0500

    Task 0

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+
+def aux_det_mat(matrix, mul):
+    """
+    Auxiliar function that calculate the determinand of a matrix by
+    by accumulatin values
+    """
+
+    width = len(matrix)
+    if width == 1:
+        return mul * matrix[0][0]
+    else:
+        sign = -1
+        sum = 0
+        for i in range(width):
+            m = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                m.append(buff)
+            sign *= -1
+            sum += mul * aux_det_mat(m, sign * matrix[0][i])
+        return sum
+
+
+def determinant(matrix):
+
+    """
+    Calculates the determinant of a matrix:
+    Args:
+        - matrix:       list of lists whose determinant should be calculated
+                - If matrix is not a list of lists, raise a TypeError with
+                  the message matrix must be a list of lists.
+                - If matrix is not square, raise a ValueError with the message
+                  matrix must be a square matrix
+                The list [[]] represents a 0x0 matrix
+    Returns:
+        - the determinant of matrix
+    """
+
+    # validate list of list
+    _is = isinstance
+
+    if not _is(matrix, list) or not any(_is(row, list) for row in matrix):
+        raise TypeError('matrix must be a list of lists')
+
+    if len(matrix[0]) == 0:
+        return 1
+
+    if len(matrix[0]) == 1:
+        return matrix[0][0]
+
+    if len(matrix) != len(matrix[0]) and len(matrix[0]):
+        raise ValueError('matrix must be a square matrix')
+
+    return aux_det_mat(matrix, 1)
