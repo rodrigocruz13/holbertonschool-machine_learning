a97c5ce054e5cc68423ad641f9f8406e1274b8c8
rodrigocruz13 <rodrigocruz13>
2020-05-06 22:46:18 -0500

    task 0

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -42,32 +42,30 @@ def determinant(matrix):
         - the determinant of matrix
     """
 
-    # validate list of list
-    _is = isinstance
-    if not _is(matrix, list):
-        raise TypeError('matrix must be a list of lists')
-
+    # there is no matrix
     if not matrix:
         raise TypeError('matrix must be a list of lists')
 
+    # the shell is not a list
+    if not isinstance(matrix, list):
+        raise TypeError('matrix must be a list of lists')
+
+    # each row has to be a list
     for row in matrix:
-        if not _is(row, list):
+        if not isinstance(row, list):
             raise TypeError('matrix must be a list of lists')
 
-    # list with one row but that row is empty
+    # matrix with one row but that row is empty
     if len(matrix) == 1 and len(matrix[0]) == 0:
         return 1
 
     # list with one row and 1 element
-    if len(matrix[0]) == 1 and len(matrix[0]) == 1:
+    if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
 
     # matrix wih size m x n
     for row in matrix:
         if len(matrix) != len(row):
             raise ValueError('matrix must be a square matrix')
-
-    if len(matrix) == 1:
-        return matrix[0][0]
 
     return aux_det(matrix, len(matrix))
