1a1a8cf43bcf463cf2cf27e039221255b16b3b3a
rodrigocruz13 <rodrigocruz13>
2020-05-11 23:40:25 -0500

    Task 3

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -50,3 +50,46 @@ class MultiNormal:
 
         self.mean = mean.T
         self.cov = cov
+
+    def pdf(self, x):
+        """
+        Method that calculates the PDF at a data point:
+        Args:
+            - x:    np.ndarray  Array of shape (d, 1) with the data point
+                                to calculate PDF
+                    d           Int. number of dimensions of the Multinomial
+                                instance
+            If x is not a numpy.ndarray, raise a TypeError with the message
+            x must by a numpy.ndarray
+            If x is not of shape (d, 1), raise a ValueError with the message
+            x mush have the shape ({d}, 1)
+
+        Returns: The value of the PDF
+        """
+
+        d = self.cov.shape[0]
+
+        if(isinstance(x, type(None))):
+            raise TypeError('x must be a numpy.ndarray')
+
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        if (x.shape[0] != d):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        if (len(x.shape) != 2) or (x.shape[1] != 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        # source:
+        # https://peterroelants.github.io/posts/multivariate-normal-primer/
+        # p(x∣μ,Σ)=[1√(2π)^d|Σ|] * exp((−1/2)(x−μ)^T(Σ^−1(x−μ))
+
+        mean = self.mean
+        cov = self.cov
+
+        x_m = x - mean
+        pdf = (1. / (np.sqrt((2 * np.pi)**d * np.linalg.det(cov)))
+               * np.exp(-(np.linalg.solve(cov, x_m).T.dot(x_m)) / 2))
+
+        return pdf[0][0]
