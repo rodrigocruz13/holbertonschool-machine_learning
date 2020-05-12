351bf2a28d908977e751cf1f236c17c9ec094d1a
rodrigocruz13 <rodrigocruz13>
2020-05-11 22:17:43 -0500

    Task 2

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -31,9 +31,8 @@ class MultiNormal:
             matrix data
         """
 
-        """if(isinstance(data, type(None))):
+        if(isinstance(data, type(None))):
             raise TypeError('data must be a 2D numpy.ndarray')
-        """
 
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
@@ -41,15 +40,13 @@ class MultiNormal:
         if data.shape[1] < 2:
             raise TypeError("data must contain multiple data points")
 
-        # Set the public instance variables
+        data = data.T
+        mean = data.mean(axis=0)
+        mean = np.reshape(mean, (-1, data.shape[1]))
 
-        d = data.shape[0]
-        n = data.shape[1] - 1
-
-        mean = np.mean(data.T, axis=0).reshape(1, d)
-        x = data.T - mean
+        n = data.shape[0] - 1
+        x = data - mean
         cov = np.dot(x.T, x) / n
 
-        self.d = d
-        self.mean = mean.T
-        self.cov = cov.T
+        self.mean = mean
+        self.cov = cov
