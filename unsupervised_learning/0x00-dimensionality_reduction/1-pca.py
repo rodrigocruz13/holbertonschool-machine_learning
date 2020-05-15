d7bbacad5abfa9c64f1d6f147dc46e3aa31ff633
rodrigocruz13 <rodrigocruz13>
2020-05-15 14:51:06 -0500

    Task 1

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+PCA: principal components analysis
+"""
+
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Function that performs PCA on a dataset:
+
+    Args:
+    - X         numpy.ndarray   Array of shape (n, d) where:
+                - n     (int)       number of data points
+                - d     (int)       number of dims in each point
+                Note: All dims have a mean of 0 across all data points
+    - ndim      (int)           New dimensionality of the transformed X
+
+    Returns:    (The weights matrix that maintains var fraction of X‘s
+                original variance)
+    T:          numpy.ndarray of shape (d, ndim) containing the
+                transformed version of X
+    """
+
+    # 1. Normalize
+    normal = np.mean(X, axis=0)
+    X_normal = X - normal
+
+    # 2. calculate the single value decomposition
+    vectors_horizontal = np.linalg.svd(X_normal)[2]
+
+    # 3. filter according ndim
+    res = vectors_horizontal[: ndim].T
+    T = X_normal @ res
+
+    return T
