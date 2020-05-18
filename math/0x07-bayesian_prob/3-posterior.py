37eff68a5806cbdb7bb237cefd2ce17be09e8d9d
rodrigocruz13 <rodrigocruz13>
2020-05-17 22:28:59 -0500

    Task 3

--- math/0x07-bayesian_prob/3-posterior.py
+++ math/0x07-bayesian_prob/3-posterior.py
@@ -6,23 +6,6 @@ Module used to
 import numpy as np
 
 
-# Factorial of a number using recursion
-
-def fact(n):
-    """
-    Function that calculates the factorial of a positive integer:
-    Args:
-    - n     int                 number of patients that develop side effects
-    Returns
-            int                 factorial of n
-    """
-
-    if n == 1:
-        return n
-    else:
-        return n * fact(n - 1)
-
-
 def n_choose_x(n, x):
     """
     Function that calculates the factorial of a positive integer:
@@ -34,9 +17,9 @@ def n_choose_x(n, x):
                                 elements from a fixed set of n elements
     """
 
-    n_fact = fact(n)
-    x_fact = fact(x)
-    nx_fact = fact(n - x)
+    n_fact = np.math.factorial(n)
+    x_fact = np.math.factorial(x)
+    nx_fact = np.math.factorial(n - x)
     return n_fact / (x_fact * nx_fact)
 
 
@@ -62,35 +45,24 @@ def likelihood(x, n, P):
         P must be a 1D numpy.ndarray
         - If any value in P is not in the range [0, 1], raise a ValueError
         with the message All values in P must be in the range [0, 1]
-
     Returns:    A 1D numpy.ndarray containing the likelihood of obtaining the
                 data, x and n, for each probability in P, respectively
     """
 
-    if not isinstance(n, int):
+    if not isinstance(n, (int, float)) or n <= 0:
         raise ValueError("n must be a positive integer")
 
-    if (n < 0):
-        raise ValueError("n must be a positive integer")
+    if not isinstance(x, (int, float)) or x < 0:
+        message = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(message)
 
-    if not isinstance(x, int):
-        raise ValueError(
-            "x must be an integer that is greater than or equal to 0")
-
-    if not (x >= 0):
-        raise ValueError(
-            "x must be an integer that is greater than or equal to 0")
-
-    if (x > n):
+    if x > n:
         raise ValueError("x cannot be greater than n")
 
-    if not isinstance(P, np.ndarray):
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
         raise TypeError("P must be a 1D numpy.ndarray")
 
-    if (P.ndim != 1):
-        raise TypeError("P must be a 1D numpy.ndarray")
-
-    if (0 < P.any() > 1):
+    if np.any(P > 1) or np.any(P < 0):
         raise ValueError("All values in P must be in the range [0, 1]")
 
     binomial_coef = n_choose_x(n, x)
@@ -141,10 +113,13 @@ def intersection(x, n, P, Pr):
     if not isinstance(Pr, np.ndarray):
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
     if (Pr.shape != P.shape):
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
-    if (0 < Pr.any() > 1):
+    if np.any(Pr > 1) or np.any(Pr < 0):
         raise ValueError("All values in Pr must be in the range [0, 1]")
 
     if not (np.isclose(np.sum(Pr), 1)):
@@ -169,8 +144,38 @@ def marginal(x, n, P, Pr):
     - Pr    1D numpy.ndarray    containing the prior beliefs of P
     Returns: the marginal probability of obtaining x and n
     """
+    if not isinstance(n, int) or (n <= 0):
+        raise ValueError("n must be a positive integer")
 
-    return sum(intersection(x, n, P, Pr))
+    if not isinstance(x, int) or (x < 0):
+        message = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(message)
+
+    if (x > n):
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if not isinstance(Pr, np.ndarray):
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if (Pr.shape != P.shape):
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if np.any(Pr > 1) or np.any(Pr < 0):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    if not (np.isclose(np.sum(Pr), 1)):
+        raise ValueError("Pr must sum to 1")
+
+    return np.sum(intersection(x, n, P, Pr))
 
 
 def posterior(x, n, P, Pr):
@@ -188,4 +193,4 @@ def posterior(x, n, P, Pr):
 
     i = intersection(x, n, P, Pr)
 
-    return i / sum(i)
+    return i / np.sum(i)
