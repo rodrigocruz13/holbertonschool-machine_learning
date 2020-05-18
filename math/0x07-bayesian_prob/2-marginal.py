37cb882c62d298983af2de7632c273e8a23cfeb2
rodrigocruz13 <rodrigocruz13>
2020-05-17 22:18:55 -0500

    Task 2

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -145,6 +145,9 @@ def marginal(x, n, P, Pr):
     Returns: the marginal probability of obtaining x and n
     """
 
+    if (Pr.shape != P.shape):
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
     if np.any(Pr > 1) or np.any(Pr < 0):
         raise ValueError("All values in Pr must be in the range [0, 1]")
 
