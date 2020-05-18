1b89753506d6045d277f9c383ca702033cf8082d
rodrigocruz13 <rodrigocruz13>
2020-05-17 22:22:43 -0500

    Task 2

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -144,5 +144,35 @@ def marginal(x, n, P, Pr):
     - Pr    1D numpy.ndarray    containing the prior beliefs of P
     Returns: the marginal probability of obtaining x and n
     """
+    if not isinstance(n, (int, float)) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, (int, float)) or x < 0:
+        message = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(message)
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if not isinstance(Pr, np.ndarray):
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if (Pr.shape != P.shape):
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if np.any(Pr > 1) or np.any(Pr < 0):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    if not (np.isclose(np.sum(Pr), 1)):
+        raise ValueError("Pr must sum to 1")
 
     return sum(intersection(x, n, P, Pr))
