2effce81e52718cd97fb8a176c857e53ed87c8e7
rodrigocruz13 <rodrigocruz13>
2020-05-19 03:01:49 -0500

    Task 0

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+PCA: principal components analysis
+"""
+
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    Funtion that initializes cluster centroids for K-means:
+
+    Args:
+    - X         numpy.ndarray       Array of shape (n, d) containing the
+                                    dataset that will be used for K-means
+                                    clustering
+        - n     int                 Number of data points
+        - d     int                 Number of dimensions for each data point
+
+    - k         int                 Positive integer containing the number
+                                    of clusters
+
+    The cluster centroids should be initialized with a multivariate uniform
+    distribution along each dimension in d:
+        * The minimum values for the distribution should be the minimum
+          values of X along each dimension in d
+        * The maximum values for the distribution should be the maximum
+          values of X along each dimension in d
+    You should use numpy.random.uniform exactly once
+
+    Returns:
+    A numpy.ndarray of shape (k, d) containing the initialized centroids
+    for each cluster, or None on failure
+    """
+
+    d = X.shape[1]
+
+    low = np.amin(X)
+    high = np.amax(X)
+
+    # generate n random points in d dimensions with elements
+    # in [-deviation, deviation]
+
+    # generate k points in d dimensions with elements in [0, max_value]
+    cluster_centers = np.random.uniform(low=low, high=high, size=(k, d))
+
+    return cluster_centers
