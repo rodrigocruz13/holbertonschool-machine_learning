d1966972350ba29f612aba4fed03623f9f660d8e
rodrigocruz13 <rodrigocruz13>
2020-05-21 17:58:35 -0500

    Refactor

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -52,49 +52,6 @@ def initialize(X, k):
     return CC
 
 
-def asign_clusters(X, distances, k, new_centers):
-    """
-    Funtion that assings to a data point the number of the closer cluster
-    according to the distance calculated
-
-    Args
-    - X             numpy.ndarray       Array of shape (n, d) containing the
-                                        dataset
-        - n         int                 Number of data points
-        - d         int                 Number of dim for each data point
-
-    - k             int                 Positive integer containing the number
-                                        of clusters
-    - distances     numpy.ndarray       Array of shape (n, k) with the distance
-                                        of each point to each cluster
-
-    Returns
-    - clusters     list                 list of shape (n, ) witht the assigned
-                                        cluster number for each datapoint
-    """
-
-    clusters = np.argmin(distances, axis=1)
-    used_clusters = np.unique(clusters, return_counts=True)[0]
-    clusters_with_no_data = np.setdiff1d(np.array(range(k)), used_clusters)
-
-    # If a cluster has no data points, reinitialize its centroid
-    while (len(clusters_with_no_data) > 0):
-
-        for i in clusters_with_no_data:
-            temp_centers = initialize(X, k)
-            new_centers[i] = temp_centers[(i - 1) % k]
-            # new_centers[i] = temp_centers[i]
-
-        # asign each datapoint to a clusters
-        deltas = X[:, np.newaxis] - new_centers
-        distances = np.sqrt(np.sum((deltas) ** 2, axis=2))
-
-        clusters = np.argmin(distances, axis=1)
-        used_clusters = np.unique(clusters, return_counts=True)[0]
-        clusters_with_no_data = np.setdiff1d(np.array(range(k)), used_clusters)
-    return clusters
-
-
 def kmeans(X, k, iterations=1000):
     """
     Function that performs K-means on a dataset:
@@ -162,7 +119,6 @@ def kmeans(X, k, iterations=1000):
             for j in range(k):
                 # If a cluster has no data points, reinitialize its centroid
                 if (X[clusters == j].size == 0):
-                    print("Cluster with no data",)
                     d = X.shape[1]
                     min_ = np.amin(X, axis=0)
                     max_ = np.amax(X, axis=0)
