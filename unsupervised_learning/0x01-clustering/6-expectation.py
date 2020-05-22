d1940655fa25bee83d830f46ba9a5c1cdfe35124
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-05-22 15:22:51 -0500

    Fix. Added cases

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -34,6 +34,9 @@ def expectation(X, pi, m, S):
         if (not isinstance(X, np.ndarray)):
             return None, None
 
+        if (not isinstance(pi, np.ndarray)):
+            return None, None
+
         if (not isinstance(m, np.ndarray)):
             return None, None
 
@@ -43,18 +46,15 @@ def expectation(X, pi, m, S):
         if (X.ndim != 2) or (m.ndim != 2) or (S.ndim != 3):
             return None, None
 
-        k = pi.shape[0]
-        if (pi.shape != (k,)):
-            return None, None
-
         if (pi.sum() != 1):
             return None, None
 
         n, d = X.shape
-        if (m.shape != (k, d)) or (S.shape != (k, d, d)):
+        k = pi.shape[0]
+        if (pi.shape != (k,)) or (m.shape != (k, d)) or (S.shape != (k, d, d)):
             return None, None
 
-        if (n < 1) or (d < 1):
+        if (n < 1) or (d < 1) or (k < 1) or (k > n):
             return None, None
 
         # https://people.duke.edu/~ccc14/sta-663/EMAlgorithm.html
