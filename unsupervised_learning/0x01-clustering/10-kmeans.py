eaace0f627e15f39f43e91a2ea9210ad7d04b5c5
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-05-22 21:47:05 -0500

    Task 10

---
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+PCA: principal components analysis
+"""
+
+import sklearn.cluster as skl
+
+
+def kmeans(X, k):
+    """
+    Function that performs K-means on a dataset:
+
+    Args:
+    - X             numpy.ndarray       Array of shape (n, d) containing the
+                                        dataset
+        - n         int                 Number of data points
+        - d         int                 Number of dim for each data point
+
+    - k             int                 Positive integer containing the number
+                                        of clusters
+
+    Returns:
+    C, clss,   or    None, None on failure
+
+    - C             np.ndarray          Array of shape (k, d) containing the
+                                        centroid means for each cluster
+    - clss          np.ndarray          Array of shape (n,) containing the
+                                        index of the cluster in C that each
+                                        data point belongs to
+    """
+
+    # Validations
+
+    kmeans = skl.KMeans(n_clusters=k).fit(X)
+    centers = kmeans.cluster_centers_
+    clusters_classes = kmeans.labels_
+
+    return centers, clusters_classes
