e21d4a19100b404e07deae57bf363369b0e3a7d5
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-05-22 21:05:30 -0500

    Task 7

---
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+PCA: principal components analysis
+"""
+
+import numpy as np
+
+pdf = __import__('5-pdf').pdf
+
+
+def maximization(X, g):
+    """
+    Function that calculates the maximization step in the EM algorithm 4 a GMM
+
+    Args:
+    - X     numpy.ndarray       Array of shape (n, d) containing the data
+    - g     numpy.ndarray       Array of shape (k, n) containing the posterior
+                                probabilities for each data point in each
+                                cluster
+    Returns:
+    pi, m, S, or None, None, None on failure
+
+    - pi    numpy.ndarray       Array of shape (k,) containing the updated
+                                priors for each cluster
+    - m     numpy.ndarray       Array of shape (k, d) containing the updated
+                                centroid means for each cluster
+    - S     numpy.ndarray       Array of shape (k, d, d) containing the
+                                updated covariance matrices for each cluster
+    """
+
+    try:
+        if (not isinstance(X, np.ndarray)):
+            return None, None, None
+
+        if (not isinstance(g, np.ndarray)):
+            return None, None, None
+
+        if not (np.isclose(np.sum(g, axis=0), 1).all):
+            return None, None, None
+
+        n, d = X.shape
+        k = g.shape[0]
+        if (g.shape != (k,n)) or (X.shape != (n, d)):
+            return None, None, None
+
+        if (n < 1) or (d < 1) or (k < 1) or (k > n):
+            return None, None, None
+
+        pi = np.zeros((k,))
+        m = np.zeros((k, d))
+        S = np.zeros((k, d, d))
+
+        for i in range(k):
+
+            # 1. Calculate priors
+            g_i_acum = np.sum(g[i], axis=0)
+            pi[i] = g_i_acum / n
+
+            # 2 Calculate centroids
+            m[i] = np.sum(np.matmul(g[i],  X), axis=0) / g_i_acum
+
+            # 3. Calculate covariances (matrix of shape k, d d)
+            diff = (X - m[i])
+            S[i] = np.dot(g[i] * diff.T, diff) / g_i_acum
+
+            return pi, m, S
+
+    except BaseException:
+        return None, None, None
+
+"""
+n_components, n_features = means.shape
+    covariances = np.empty((n_components, n_features, n_features))
+    for k in range(n_components):
+        diff = X - means[k]
+        covariances[k] = np.dot(resp[:, k] * diff.T, diff) / nk[k]
+        covariances[k].flat[::n_features + 1] += reg_covar
+    return covariances
+
+"""
