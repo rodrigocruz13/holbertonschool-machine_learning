94f4a20f8500da7c6deeab0e7105f6d2d9b17de0
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-05-22 21:08:06 -0500

    Task 7

--- unsupervised_learning/0x01-clustering/7-main.py
+++ unsupervised_learning/0x01-clustering/7-main.py
@@ -10,13 +10,14 @@ if __name__ == '__main__':
     a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
     b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
     c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
-    d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    d = np.random.multivariate_normal(
+        [20, 70], [[35, 10], [10, 35]], size=1000)
     X = np.concatenate((a, b, c, d), axis=0)
     np.random.shuffle(X)
     pi, m, S = initialize(X, 4)
-    print ("pi = ", pi)
-    print ("   = ", m)
-    print ("S  = ", S)
+    print("pi = ", pi)
+    print("   = ", m)
+    print("S  = ", S)
 
     g, _ = expectation(X, pi, m, S)
     pi, m, S = maximization(X, g)
