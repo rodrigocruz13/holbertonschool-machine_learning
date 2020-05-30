1d4f450c9af9e0dbc9abe99f95c0340405440fd0
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-05-30 08:04:46 -0500

    Task 3

---
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+"""
+PCA: principal components analysis
+"""
+
+import numpy as np
+
+
+def markov_chain(P, s, t=1):
+    """
+    Function that determines the probability of a markov chain being in a
+    particular state after a specified number of iterations:
+
+    Args:
+    - P     np.ndarray          It is a 2d array of shape (n, n) representing
+                                the transition matrix.
+                                P[i, j] is the probability of transitioning
+                                from state i to state j
+        - n     int             Number of states in the markov chain
+    - s     np.ndarray          Array of shape (1, n) representing the prob
+                                of starting in each state
+    - t     int                 Number of iterations that the markov chain has
+                                been through
+    Returns:
+                                a numpy.ndarray of shape (1, n) representing
+                                the probability of being in a specific state
+                                after t iterations, or None on failure
+    """
+
+    try:
+
+        if (not isinstance(P, np.ndarray)) or (not isinstance(s, np.ndarray)):
+            return None
+
+        if (not isinstance(t, int)):
+            return None
+
+        if (P.ndim != 2) or (s.ndim != 2) or (t < 1):
+            return None
+
+        n = P.shape[0]
+        if (P.shape != (n, n)) or (s.shape != (1, n)):
+            return None
+
+        while (t > 0):
+            s = np.matmul(s, P)
+            t = t - 1
+        return s
+    except BaseException:
+        return None
+
+
+def regular(P):
+    """
+    Function that determines the steady state probabilities of a regular
+    markov chain:
+
+    Args:
+    - P         numpy.ndarray       A square 2D array of shape (n, n)
+                                    representing the transition matrix
+                                    P[i, j] is the probability of
+                                    transitioning from state i to state j
+    - n         int                 Number of states in the markov chain
+    Returns:
+    - regular   numpy.ndarray       Array of shape (1, n) containing the
+                                    steady state probabilities, or None on
+                                    failure
+    """
+
+    np.warnings.filterwarnings('ignore')
+    # Avoid this warning: Line 92.  np.linalg.lstsq(a, b)[0]
+
+    try:
+
+        if (not isinstance(P, np.ndarray)):
+            return None
+
+        if (P.ndim != 2):
+            return None
+
+        n = P.shape[0]
+        if (P.shape != (n, n)):
+            return None
+
+        if ((np.sum(P) / n) != 1):
+            return None
+
+        if ((P > 0).all()):  # Are all elements of P positive ?
+            a = np.eye(n) - P
+            a = np.vstack((a.T, np.ones(n)))
+            b = np.matrix([0] * n + [1]).T
+            regular = np.linalg.lstsq(a, b)[0]
+            return regular.T
+
+        return None
+
+    except BaseException:
+        return None
+
+
+def absorbing(P):
+    """
+    Function that determines determines if a markov chain is absorbing:
+
+    Args:
+    - P         numpy.ndarray       A square 2D array of shape (n, n)
+                                    representing the transition matrix
+                                    P[i, j] is the probability of
+                                    transitioning from state i to state j
+    - n         int                 Number of states in the markov chain
+    Returns:
+    - True if it is absorbing, or False on failure
+    """
+
+    np.warnings.filterwarnings('ignore')
+    # Avoid this warning: Line 92.  np.linalg.lstsq(a, b)[0]
+
+    try:
+
+        if (not isinstance(P, np.ndarray)):
+            return False
+
+        if (P.ndim != 2):
+            return False
+
+        n = P.shape[0]
+        if (P.shape != (n, n)):
+            return False
+
+        if ((np.sum(P) / n) != 1):
+            return False
+
+        # P is an identity matrix
+        identity = np.eye(n)
+        if (np.equal(P, identity).all()):
+            return True
+
+        # Some rows of P is = row of identity matrix
+        abs = np.zeros(n)
+        for i in range(n):
+            if P[i][i] == 1:
+                abs[i] = 1
+
+        prev = np.zeros(n)
+        while (not np.array_equal(abs, prev) and abs.sum() != n):
+            prev = abs.copy()
+            for absorbed in P[:, np.nonzero(abs)[0]].T:
+                abs[np.nonzero(absorbed)] = 1
+        if (abs.sum() == n):
+            return True
+        return False
+
+        return False
+
+    except BaseException as e:
+        print(e)
+        return False
+
+
+def forward(Observation, Emission, Transition, Initial):
+    """
+    Function that performs the forward algorithm for a hidden markov model:
+
+    Args:
+    - Observation       numpy.ndarray       Array of shape (T,) that contains
+                                            the index of the observation
+                T       int                 Number of observations
+    - Emission          numpy.ndarray       Array of shape (N, M) containing
+                                            the emission probability of a
+                                            specific observation given a
+                                            hidden state
+                                            - Emission[i, j] is the probability
+                                            of observing j given the hidden
+                                            state i
+            - N         int                 Number of hidden states
+            - M         int                 Number of all possible observations
+    - Transition        numpy.ndarray       2D array of shape (N, N) containing
+                                            the transition probabilities
+                                            - Transition[i, j] is the prob
+                                            of transitioning from the hidden
+                                            state i to j
+    - Initial           numpy.ndarray       Array of shape (N, 1) containing
+                                            the probability of starting in a
+                                            particular hidden state
+    Returns: P, F, or None, None on failure
+    - P:                                    likelihood of the observations
+                                            given the model
+    - F:                Numpy.ndarray       Array of shape (N, T) containing
+                                            the forward path probabilities
+                                            F[i, j] is the probability of
+                                            being in hidden state i at time j
+                                            given the previous observations
+    """
+
+    # https://tinyurl.com/ybl8y8uh
+    T = Observation.shape[0]
+    N = Emission.shape[0]
+
+    alpha = np.zeros((N, T))
+    alpha[:, 0] = Initial.T * Emission[:, Observation[0]]
+
+    for t in range(1, T):
+        for j in range(N):
+            alpha[j, t] = (alpha[:, t - 1].dot(Transition[:, j])
+                           * Emission[j, Observation[t]])
+
+    likelihood = np.sum(alpha[:, t])
+    return likelihood, alpha
