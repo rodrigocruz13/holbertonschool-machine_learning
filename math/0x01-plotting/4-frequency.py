b0dc5593eb2d5db4c3716d08c664dbd603cb77fd
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-06-07 08:09:19 -0500

    Cosmetic changes

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,15 +5,30 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
+# your code here ------------------------------>
+
+# 0 frame
+plt.figure(figsize=(10, 7)).set_facecolor("white")
+
+# labels
 plt.xlabel("Grades")
 plt.ylabel("Number of Students")
 plt.title("Project A")
 
 align = "mid"  # aligment
 edgecolor = 'black'  # edgecolor
+innercolor = 'steelblue'
+
+# 3. font
+plt.rcParams.update({'font.size': 13.5})
+
 n_bins = range(0, 110, 10)
-plt.xticks(np.arange(0, 100, step=10))
-plt.hist(student_grades, bins=n_bins, edgecolor=edgecolor)
+plt.xticks(np.arange(0, 101, step=10))
+plt.hist(student_grades,
+         bins=n_bins,
+         edgecolor=edgecolor,
+         linewidth=1.5,
+         color=innercolor)
 
 plt.xlim(0, 100)
 plt.ylim(0, 30)
