acb88c3794fc3ad70d9cb57b90a08bd6cfa671dc
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-06-07 06:18:30 -0500

    Cosmetic changes

--- math/0x01-plotting/0-line.py
+++ math/0x01-plotting/0-line.py
@@ -2,11 +2,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
 
-x = range(0, 11)
-c = "r"  # red
+y_data = np.arange(0, 11) ** 3
+# your code goes here ----------------------- >
+
+# 1. x data
+x_data = range(0, 11)
+
+# 2. Colors
+line_color = "r"  # line color red
+plt.figure(figsize=(10, 7)).set_facecolor("white")  # size & background color
+
+# 3. limits
 plt.xlim(0, 10)
+plt.ylim(-50, 1050)
 
-plt.plot(x, y, c)
+# 4. font
+plt.rcParams.update({'font.size': 16})
+
+# display
+plt.plot(x_data, y_data, line_color, linewidth=3)
+plt.grid(True)
 plt.show()
