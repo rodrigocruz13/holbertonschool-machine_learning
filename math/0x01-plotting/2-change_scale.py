327c669f166bbb34c9590b8f94a359e350212b68
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-06-07 07:22:42 -0500

    Cosmetic changes

--- math/0x01-plotting/2-change_scale.py
+++ math/0x01-plotting/2-change_scale.py
@@ -7,11 +7,25 @@ r = np.log(0.5)
 t = 5730
 y = np.exp((r / t) * x)
 
+# your code here ---------------------->
+
+# 0 frame
+plt.figure(figsize=(10, 7)).set_facecolor("white")
+
 plt.xlabel("Time (years)")
 plt.ylabel("Fraction Remaining")
 plt.title("Exponential Decay of C-14")
 
-plt.plot(x, y)
-plt.xlim(0, 28650)
+# 2. Colors
+line_color = '#67c2db'  # line color blue
+
+
+# 3. font
+plt.rcParams.update({'font.size': 16})
 plt.yscale('log')
+plt.xlim(0, 28650)
+plt.ylim(0.025, 1.25)
+
+
+plt.plot(x, y, c='steelblue', linewidth=3)
 plt.show()
