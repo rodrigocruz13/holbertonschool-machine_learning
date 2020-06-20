0e5f471ba62ee0cf2ea0e2e30d5a41ef15563e57
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-06-19 21:08:15 -0500

    Task 0

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -3,7 +3,7 @@
 Autoencoders
 """
 import numpy as np
-import tensorflow.keras as K
+import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
@@ -36,25 +36,25 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
 
     n = len(hidden_layers)
     # 1. Encoder
-    encoded = x = K.Input(shape=(input_dims, ))
+    encoded = x = keras.Input(shape=(input_dims, ))
     for i in range(n):
-        x = K.layers.Dense(hidden_layers[i], activation="relu")(x)
-    h = K.layers.Dense(latent_dims, activation="relu")(x)
-    encoder = K.models.Model(inputs=encoded, outputs=h)
+        x = keras.layers.Dense(hidden_layers[i], activation="relu")(x)
+    h = keras.layers.Dense(latent_dims, activation="relu")(x)
+    encoder = keras.models.Model(inputs=encoded, outputs=h)
 
     # 2. Decoder
-    decoded = y = K.Input(shape=(latent_dims, ))
+    decoded = y = keras.Input(shape=(latent_dims, ))
     # start = n-1, stop = -1, step = -1
     for j in range((n - 1), -1, -1):
-        y = K.layers.Dense(hidden_layers[j], activation="relu")(y)
-    r = K.layers.Dense(input_dims, activation="sigmoid")(y)
-    decoder = K.models.Model(inputs=decoded, outputs=r)
+        y = keras.layers.Dense(hidden_layers[j], activation="relu")(y)
+    r = keras.layers.Dense(input_dims, activation="sigmoid")(y)
+    decoder = keras.models.Model(inputs=decoded, outputs=r)
 
     # 3. Autoencoder
-    inputs = K.Input(shape=(input_dims, ))
+    inputs = keras.Input(shape=(input_dims, ))
     outputs = decoder(encoder(inputs))
 
-    autoencoder = K.models.Model(inputs=inputs, outputs=outputs)
+    autoencoder = keras.models.Model(inputs=inputs, outputs=outputs)
     autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
 
     return encoder, decoder, autoencoder
