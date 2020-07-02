913c77bf6b1d9ed0c7cd719d3e86bb93f0af2e44
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-07-02 13:37:19 -0500

    npz files added

--- supervised_learning/0x04-error_analysis/0-main.py
+++ supervised_learning/0x04-error_analysis/0-main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import numpy as np
-create_confusion_matrix = __import__('0-create_confusion').create_confusion_matrix
+create_confusion_matrix = __import__(
+    '0-create_confusion').create_confusion_matrix
 
 if __name__ == '__main__':
     lib = np.load('labels_logits.npz')
     labels = lib['labels']
     logits = lib['logits']
-
+    i = 5 * 6 + 1
     np.set_printoptions(suppress=True)
     confusion = create_confusion_matrix(labels, logits)
     print(confusion)
