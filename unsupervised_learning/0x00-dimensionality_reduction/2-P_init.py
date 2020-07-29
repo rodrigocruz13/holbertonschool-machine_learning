9f07c4837c1f70151c180d82e502027c13f419ac
rodrigocruz13 <rcamerica123>
2020-07-29 17:11:18 -0500

    Task 2

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -27,18 +27,20 @@ def P_init(X, perplexity):
                                     0‘s that will contain the P affinities
         - betas numpy.ndarray       Array of shape (n, 1) initialized to all
                                     1’s that will contain all of the beta
-                                    values, where
-                                    β_i = 1 / (2 (σ_i ^2))
+                                    values β_i = 1 / (2 (σ_i ^2))
         - H                         The Shannon entropy for perplexity
                                     perplexity
 
         Note:   σ_i is associated with a parameter called perplexity which
                 can be loosely interpreted as the number of close neighboor
-                each point has
+                each point has.
+        Paper:
+        http://www.jmlr.org/papers/volume9/vandermaaten08a/vandermaaten08a.pdf
     """
 
-    n = X.shape[0]
+    # symbols:  ≈ Π Σ ² ³ σ ∩ ∪ α β γ δ ε ζ η θ Φ º ~ Δ π σ ± ⋅ ÷ ∫ ∝ ∞ ⊆ ⊂ ≠
 
+    n = X.shape[0]
     P = np.zeros((n, n))
     betas = np.ones((n, 1))
 
@@ -46,6 +48,11 @@ def P_init(X, perplexity):
     # https://nlml.github.io/in-raw-numpy/in-raw-numpy-t-sne/
     sum_X = np.sum(np.square(X), 1)
     D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+
+    # https://leimao.github.io/blog/Entropy-Perplexity/
+    # Chapter. Perplexity
+    # PP(p) = b ^ (h(p)), where b is the base of the logarithm used.
+    # from there hp = log2 (PP)
 
     H = np.log2(perplexity)
 
--- unsupervised_learning/0x00-dimensionality_reduction/2-main.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-main.py
@@ -2,10 +2,17 @@
 
 import numpy as np
 pca = __import__('1-pca').pca
+P_init = __import__('2-P_init').P_init
 
 X = np.loadtxt("mnist2500_X.txt")
+X = pca(X, 50)
+D, P, betas, H = P_init(X, 30.0)
 print('X:', X.shape)
 print(X)
-T = pca(X, 50)
-print('T:', T.shape)
-print(T)
+print('D:', D.shape)
+print(D)
+print('P:', P.shape)
+print(P)
+print('betas:', betas.shape)
+print(betas)
+print('H:', H)
