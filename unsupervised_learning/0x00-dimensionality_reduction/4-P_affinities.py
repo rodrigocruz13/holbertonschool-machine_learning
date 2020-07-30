14eda5f4f82e61648ef9266eef9dc1bc48189acb
rodrigocruz13 <rcamerica123>
2020-07-30 01:39:58 -0500

    Task 4

--- unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -10,7 +10,7 @@ HP = __import__('3-entropy').HP
 
 def P_affinities(X, tol=1e-5, perplexity=30.0):
     """
-    Write a function def P_affinities(X, tol=1e-5, perplexity=30.0): that 
+    Write a function def P_affinities(X, tol=1e-5, perplexity=30.0): that
     calculates the symmetric P affinities of a data set:
 
     Arguments:
@@ -27,52 +27,48 @@ def P_affinities(X, tol=1e-5, perplexity=30.0):
     P affinities
     """
 
+    # symbols:  ≈ Π Σ ² ³ σ ∩ ∪ α β γ δ ε ζ η θ Φ º ~ Δ π σ ± ⋅ ÷ ∫ ∝ ∞ ⊆ ⊂ ≠
+
+    # source:
+    # https://en.wikipedia.org/wiki/T-distributed_stochastic_neighbor_embedding
+    # section Software and then to: https://lvdmaaten.github.io/tsne/ -> python
+
     (n, d) = X.shape
     sum_X = np.sum(np.square(X), 1)
-    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
-    P = np.zeros((n, n))
-    beta = np.ones((n, 1))
-    logU = np.log(perplexity)
+    D, P, β, logU = P_init(X, perplexity)
 
     # Loop over all datapoints
     for i in range(n):
 
-        # Print progress
-        if i % 500 == 0:
-            print("Computing P-values for point %d of %d..." % (i, n))
-
         # Compute the Gaussian kernel and entropy for the current precision
-        betamin = -np.inf
-        betamax = np.inf
-        Di = D[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))]
-        (H, thisP) = HP(Di, beta[i])
+        βmin = -np.inf
+        βmax = np.inf
+        Di = D[i, np.concatenate((np.r_[0:i], np.r_[i + 1:n]))]
+        (H, thisP) = HP(Di, β[i])
 
         # Evaluate whether the perplexity is within tolerance
         Hdiff = H - logU
-        tries = 0
-        while np.abs(Hdiff) > tol and tries < 50:
+        # tries = 0
+        # while np.abs(Hdiff) > tol and tries < 50:
+        while np.abs(Hdiff) > tol:
 
             # If not, increase or decrease precision
+            inf_range = (np.inf, -np.inf)
             if Hdiff > 0:
-                betamin = beta[i].copy()
-                if betamax == np.inf or betamax == -np.inf:
-                    beta[i] = beta[i] * 2.
-                else:
-                    beta[i] = (beta[i] + betamax) / 2.
+                βmin = β[i].copy()
+                β[i] = β[i] * 2 if (βmax in inf_range) else (β[i] + βmax) / 2
             else:
-                betamax = beta[i].copy()
-                if betamin == np.inf or betamin == -np.inf:
-                    beta[i] = beta[i] / 2.
-                else:
-                    beta[i] = (beta[i] + betamin) / 2.
+                βmax = β[i].copy()
+                β[i] = β[i] / 2 if (βmin in inf_range) else (β[i] + βmin) / 2.
 
             # Recompute the values
-            (H, thisP) = Hbeta(Di, beta[i])
-            Hdiff = H - logU
-            tries += 1
+            (Hi, thisP) = HP(Di, β[i])
+            Hdiff = Hi - logU
+            # tries += 1
 
         # Set the final row of P
-        P[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))] = thisP
-        P = np.mean(np.sqrt(1 / beta))
+        P[i, np.concatenate((np.r_[0:i], np.r_[i + 1:n]))] = thisP
 
+    # Convrting P into a symmetric matrix
+    P = (P + P.T) / (2 * n)
     return P
