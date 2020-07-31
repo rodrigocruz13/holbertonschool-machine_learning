898a84dd279016f0c779a21ef0f714b5d8cc9ff8
rodrigocruz13 <rcamerica123>
2020-07-31 18:14:54 -0500

    task 1

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -49,20 +49,20 @@ def likelihood(x, n, P):
                 data, x and n, for each probability in P, respectively
     """
 
-    if not isinstance(n, (int, float)) or n <= 0:
+    if (not isinstance(n, (int, float)) or n <= 0):
         raise ValueError("n must be a positive integer")
 
-    if not isinstance(x, (int, float)) or x < 0:
+    if (not isinstance(x, (int, float)) or x < 0):
         message = "x must be an integer that is greater than or equal to 0"
         raise ValueError(message)
 
-    if x > n:
+    if (x > n):
         raise ValueError("x cannot be greater than n")
 
-    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
+    if (not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1):
         raise TypeError("P must be a 1D numpy.ndarray")
 
-    if np.any(P > 1) or np.any(P < 0):
+    if (np.any(P > 1) or np.any(P < 0)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
     binomial_coef = n_choose_x(n, x)
@@ -110,19 +110,19 @@ def intersection(x, n, P, Pr):
     Returns:    a 1D numpy.ndarray containing the intersection
     """
 
-    if not isinstance(Pr, np.ndarray):
+    if (not isinstance(Pr, np.ndarray)):
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
-    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
+    if (not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1):
         raise TypeError("P must be a 1D numpy.ndarray")
 
     if (Pr.shape != P.shape):
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
-    if np.any(Pr > 1) or np.any(Pr < 0):
+    if (np.any(Pr > 1) or np.any(Pr < 0)):
         raise ValueError("All values in Pr must be in the range [0, 1]")
 
-    if not (np.isclose(np.sum(Pr), 1)):
+    if (not np.isclose(np.sum(Pr), 1)):
         raise ValueError("Pr must sum to 1")
 
     # If events are independent , P(A ∩ B) = P(A) * P(B)
