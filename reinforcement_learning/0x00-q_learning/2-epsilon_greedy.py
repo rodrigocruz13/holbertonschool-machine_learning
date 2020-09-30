77f9fd8cd94efab169d259ee65169592fbe25836
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-09-29 21:47:31 -0500

    Task 2 added

---
+++ reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Module used to
+"""
+
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """[summary]
+
+    Args:
+        Q ([numpy.ndarray]): [array containing the q-table]
+        state ([type]): [description]
+        epsilon ([type]): [description]
+    """
+
+    # https://www.youtube.com/watch?v=HGeI30uATws&list=PLZbbT5o_s2xoWNVdDudn51XM8lOuZ_Njv&index=9
+    ε = epsilon
+    # print(Q)
+    # print(Q.shape)
+    # print(Q[state])
+    # print (state, epsilon)
+
+    to_explore = np.random.randint(Q.shape[1])
+    to_exploit = np.argmax(Q[state, :])
+    exploration_rate_threshold = np.random.uniform(0, 1)
+
+    movement = to_explore if ε > exploration_rate_threshold else to_exploit
+
+    return movement
