51296762711e3a2d5d98505eb0b84633affbcbfe
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-10-01 23:48:13 -0500

    Main fi

---
+++ supervised_learning/0x11-attention/7-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+
+eblock = EncoderBlock(512, 8, 2048)
+print(eblock.mha)
+print(eblock.dense_hidden)
+print(eblock.dense_output)
+print(eblock.layernorm1)
+print(eblock.layernorm2)
+print(eblock.dropout1)
+print(eblock.dropout2)
+x = tf.random.uniform((32, 10, 512))
+output = eblock(x, True, None)
+print(output)
