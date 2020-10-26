032c9e461e73be90b0b8497eeb798681a1e17ad6
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-10-26 15:29:32 -0500

    Task 5

---
+++ pipeline/0x00-pandas/5-slice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df.iloc[::60, 2:6]  # start, stop, step, col_ini: col_end
+
+
+print(df.tail())
