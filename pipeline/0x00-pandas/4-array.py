c04755cb689a6697e4f8349aba9c1f8133933994
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-10-26 15:15:50 -0500

    Task 4

---
+++ pipeline/0x00-pandas/4-array.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+# Your code here
+
+cols = ["High", "Close"]
+A = df.loc[:, cols].tail(10).to_numpy()
+
+print(A)
