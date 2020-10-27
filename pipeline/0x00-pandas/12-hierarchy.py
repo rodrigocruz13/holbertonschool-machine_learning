b8e56f6850d8b53b0a2925f15a02bb04087c0cbc
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-10-26 20:52:55 -0500

    Task 12

---
+++ pipeline/0x00-pandas/12-hierarchy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
+
+# YOUR CODE HERE
+
+df1 = df1.set_index('Timestamp')
+df2 = df2.set_index('Timestamp')
+
+df1 = df1.sort_values(by='Timestamp', ascending=True)
+df2 = df1.loc["1417411980":"1417417980"]
+
+df2 = df2.sort_values(by='Timestamp', ascending=True)
+df2 = df2.loc["1417411980":"1417417980"]
+
+objects = [df2, df1]
+keys = ["bitstamp", "coinbase"]
+
+df = pd.concat(objects, keys=keys)
+
+df = df.swaplevel(0, 1, axis=0).sort_index()
+
+print(df)
