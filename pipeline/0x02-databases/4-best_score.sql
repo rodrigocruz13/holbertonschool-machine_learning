2a911f1710543e995132de4048f373afe157125e
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-11-02 21:17:01 -0500

    Task 4

--- pipeline/0x02-databases/4-best_score.sql
+++ pipeline/0x02-databases/4-best_score.sql
@@ -1,2 +1,3 @@
 -- This lists all records of the table second_table of the database hbtn_0c_0 and score >=10.
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table
+WHERE score >= 10 ORDER BY score DESC;
