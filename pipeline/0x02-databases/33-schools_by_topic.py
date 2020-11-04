c0051cd1a016a0794ffb0172b5a598d2ae530fc0
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-11-04 17:53:33 -0500

    Task 33

---
+++ pipeline/0x02-databases/33-schools_by_topic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Module used to
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """[changes all topics of a school document based on the name]
+
+    Args:
+        mongo_collection ([db]): [mongo collection object]
+        topics ([list of strings]): [list of topics approached in the school]
+
+    Returns:
+        list of school having a specific topic
+    """
+
+    # create the data dictionary
+
+    all_ = mongo_collection.find({"topics": {"$all": [topic]}})
+    #print(type(all_))
+    #print("--------------------")
+    return [school for school in all_]
