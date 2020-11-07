95219ca66c6cd61c7aa9a3f7d9ff519a2cea95bd
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-11-07 01:31:29 -0500

    Fix. Added documentation

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """ Module used to
 """
-
 from pymongo import MongoClient
+
+"""
+[summary]
+"""
 
 method_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 db_address = 'mongodb://127.0.0.1:27017'
@@ -11,7 +14,7 @@ client = MongoClient(db_address)
 
 # create dict of all dbs and its collections
 d = dict((db, [collection for collection in client[db].collection_names()])
-         for db in client.database_names())
+        for db in client.database_names())
 
 # validate logs and nginx exists
 if ('logs' not in d.keys() or 'nginx' != d['logs'][0]):
@@ -19,12 +22,13 @@ if ('logs' not in d.keys() or 'nginx' != d['logs'][0]):
 
 collection = client.logs.nginx  # client.database.collection
 docs = collection.count_documents({})
-print(docs, "logs")
+print("{} logs".format(docs))
 
 print("Methods:")
 for method in method_list:
     count = collection.count_documents({"method": method})
     print("\tmethod {}: {}".format(method, count))
 
-GET_status = collection.count_documents({"method": "GET", "path": "/status"})
-print(GET_status, "status check")
+d = {"method": "GET", "path": "/status"}
+GET_status = collection.count_documents(d)
+print("{} status check".format(GET_status))
